3b21caf500ce19e4815686dc3a364a7d2244b7d0
yoshiki-447 <arai-yoshiki366@g.ecc.u-tokyo.ac.jp>
2024-11-29 20:33:51 +0900

    wake heavy manequine

--- rosbag/rosbag.sh
+++ rosbag/rosbag.sh
@@ -7,6 +7,6 @@ TIMESTAMP=$(date "+%Y%m%d_%H%M%S")
 OUTPUT_DIR=$(rospack find jaxon_transfer)/rosbag
 OUTPUT_FILE="rosbag_$TIMESTAMP.bag"
 
-TOPICS="/joint_states /tf /tf_static /off_rhsensor /off_lhsensor /clock"
+TOPICS="/joint_states /tf /tf_static /off_rhsensor /off_lhsensor /clock /ref_rh_wrench /ref_lh_wrench"
 
 rosbag record -O $OUTPUT_DIR/$OUTPUT_FILE $TOPICS 
